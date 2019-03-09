117d0bffdb988a023127498c38b5e22458ee5f02
Alberto Garcia <albertog@icmab.es>
2019-03-10 00:33:13 +0100

    Actually add broadcast_fdf_struct

---
+++ Src/broadcast_fdf_struct.F90
@@ -0,0 +1,58 @@
+!
+!   Broadcast complete fdf structure
+!
+    SUBROUTINE broadcast_fdf_struct(reading_node,comm)
+
+#ifndef MPI
+      implicit none
+
+      integer, intent(in)       :: reading_node         ! Node which contains the struct
+      integer, intent(in)       :: comm
+#else
+      use mpi
+
+      use fdf, only: serialize_fdf_struct, recreate_fdf_struct
+      use fdf, only: size_of_fdf_struct, set_fdf_started
+      
+      implicit none
+
+      integer, intent(in)       :: reading_node         ! Node which contains the struct
+      integer, intent(in)       :: comm
+
+      character, allocatable    :: bufferFDF(:)
+      integer                   :: ierr, nchars, rank
+
+      call MPI_Comm_Rank( Comm, rank, ierr )
+      if (rank == reading_node) then
+         nchars = size_of_fdf_struct()
+      endif
+
+      call MPI_Bcast(nchars, 1,                                 &
+                     MPI_INTEGER, reading_node, comm, ierr)
+      if (ierr .ne. MPI_SUCCESS) then
+        call die("Error broadcasting size of fdf struct")
+      endif
+
+      ALLOCATE(bufferFDF(nchars), stat=ierr)
+      if (ierr .ne. 0) then
+        call die("Error allocating buffer")
+      endif
+
+      if (rank == reading_node) then
+         call serialize_fdf_struct(bufferFDF)
+      endif
+
+      call MPI_Bcast(bufferFDF, size(bufferFDF),              &
+                     MPI_CHARACTER, reading_node, MPI_COMM_WORLD, ierr)
+      if (ierr .ne. MPI_SUCCESS) then
+        call die("Error Broadcasting bufferFDF")
+      endif
+
+      if (rank /= reading_node) then
+         call recreate_fdf_struct(bufferFDF)
+         call set_fdf_started(.true.)
+      endif
+
+      DEALLOCATE(bufferFDF)
+#endif
+    END SUBROUTINE broadcast_fdf_struct
