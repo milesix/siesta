6544c6a42901b067ab8e1207321439572b4333e4
Alberto Garcia <albertog@icmab.es>
2020-05-24 23:02:10 +0200

    Almost functional

--- Util/matel-test/matel-test.f90
+++ Util/matel-test/matel-test.f90
@@ -16,10 +16,15 @@ program matel_test
 
       use m_ion_io
       use atm_types, only: species, nspecies
+      use m_matel_registry, only: show_pool
+      use matel_mod, only: init_matel, new_matel
       
       implicit none
 
+      
       character(len=20), allocatable, dimension(:) :: species_label
+      integer :: io
+      real(dp) :: val, grad(3)
       
       nspecies = 1
       allocate(species_label(1))
@@ -30,8 +35,20 @@ program matel_test
 
       write(*,*) species(1)%label
       
-!      call register_rfs()
+      call register_rfs()
+      call show_pool()
+      call init_matel()
 
+      io = species(1)%orb_gindex(1)
+
+      call new_MATEL('S', io, io, [ 1.0_dp, 0.0_dp, 0.0_dp ], val, grad)
+      print "(a,f10.4,2x,3f10.4)", "<3s|  | 3s(1.0)> : ", val, grad
+      call new_MATEL('X', io, io, [ 1.0_dp, 0.0_dp, 0.0_dp ], val, grad)
+      print "(a,f10.4,2x,3f10.4)", "<3s| X | 3s(1.0)> : ", val, grad
+      call new_MATEL('Y', io, io, [ 1.0_dp, 0.0_dp, 0.0_dp ], val, grad)
+      print "(a,f10.4,2x,3f10.4)", "<3s| Y | 3s(1.0)> : ", val, grad
+      call new_MATEL('Z', io, io, [ 1.0_dp, 0.0_dp, 0.0_dp ], val, grad)
+      print "(a,f10.4,2x,3f10.4)", "<3s| Z | 3s(1.0)> : ", val, grad
     end program  matel_test
 
 
