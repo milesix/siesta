22cc49c376002cc3e518f1bc2825286f3eb0d1be
Alberto Garcia <albertog@icmab.es>
2020-06-28 11:55:39 +0200

    First working version with Lua

--- Src/m_genq.f90
+++ Src/m_genq.f90
@@ -1,6 +1,162 @@
 !
+module m_genq_lua
+  use flook
+
+  private
+  
+  integer, parameter :: dp = selected_real_kind(10,100)
+  
+  type(luaState), public :: lua
+  public :: genq_lua_initialize
+
+  integer, public :: natoms, nqs
+  
+  integer, allocatable, public  :: isa(:)
+  real(dp), allocatable, public    :: q(:) ! could use setter
+  real(dp), allocatable, public :: xa(:,:)
+  
+  logical, public :: genq_lua_initialized = .false.
+
+CONTAINS
+
+  subroutine genq_lua_initialize()
+    ! This routine reads the Lua script, registers a few
+    ! functions to exchange data (Fortran arrays/Lua tables)
+    ! and gets the number of generalized coordinates and atoms
+    ! that the Lua side knows about
+    
+  if (genq_lua_initialized) RETURN
+    
+  ! Initialize the @lua environment
+  call lua_init(lua)
+
+  ! Register a couple of functions to pass information back and
+  ! forth between @lua.
+  call lua_register(lua, 'fortran_set_dimensions', script_get_dimensions )
+  call lua_register(lua, 'fortran_get_qs', script_set_qs )
+  call lua_register(lua, 'fortran_set_xa', script_get_xa )
+  call lua_register(lua, 'fortran_set_isa', script_get_isa )
+
+  call lua_run(lua, 'genq_flook.lua' )
+
+  call lua_run(lua, code = 'fortran_set_dimensions()')
+  
+  allocate(q(nqs))
+  allocate(xa(3,natoms), isa(natoms))
+
+  genq_lua_initialized = .true.
+  
+end subroutine genq_lua_initialize
+
+  function script_set_qs(state) result(nret)
+    use, intrinsic :: iso_c_binding, only: c_ptr, c_int
+    
+    type(c_ptr), value :: state
+    ! Define the in/out
+    integer(c_int) :: nret
+
+    type(luaState) :: lua
+    type(luaTbl) :: tbl
+
+    call lua_init(lua,state)
+
+    ! open global table in variable genq
+    tbl = lua_table(lua,'genq')
+
+    ! Set the variables to the genq table:
+    call lua_set(tbl,'q',q)
+
+    call lua_close_tree(tbl)
+
+    ! this function returns nothing
+    nret = 0
+    
+  end function script_set_qs
+
+  function script_get_xa(state) result(nret)
+    use, intrinsic :: iso_c_binding, only: c_ptr, c_int
+    
+    ! Define the state
+    type(c_ptr), value :: state
+    ! Define the in/out
+    integer(c_int) :: nret
+
+    type(luaState) :: lua
+    type(luaTbl) :: tbl
+
+    call lua_init(lua,state)
+
+    ! open global table in variable genq
+    tbl = lua_table(lua,'genq')
+
+    ! Get the variables from the genq table:
+    call lua_get(tbl,'xa',xa)
+
+    call lua_close_tree(tbl)
+
+    ! this function returns nothing
+    nret = 0
+    
+  end function script_get_xa
+
+function script_get_isa(state) result(nret)
+  use, intrinsic :: iso_c_binding, only: c_ptr, c_int
+
+  ! Define the state
+  type(c_ptr), value :: state
+  ! Define the in/out
+  integer(c_int) :: nret
+
+  type(luaState) :: lua
+  type(luaTbl) :: tbl
+
+  call lua_init(lua,state)
+
+  ! open global table in variable genq
+  tbl = lua_table(lua,'genq')
+
+  ! Get the variables from the genq table:
+  call lua_get(tbl,'species',isa)
+
+  call lua_close_tree(tbl)
+
+  ! this function returns nothing
+  nret = 0
+
+end function script_get_isa
+
+  function script_get_dimensions(state) result(nret)
+    use, intrinsic :: iso_c_binding, only: c_ptr, c_int
+    
+    ! Define the state
+    type(c_ptr), value :: state
+    ! Define the in/out
+    integer(c_int) :: nret
+
+    type(luaState) :: lua
+    type(luaTbl) :: tbl
+
+    call lua_init(lua,state)
+
+    ! open global table in variable genq
+    tbl = lua_table(lua,'genq')
+
+    ! Get the variables from the genq table:
+    call lua_get(tbl,'nqs',nqs)
+    call lua_get(tbl,'natoms',natoms)
+
+    call lua_close_tree(tbl)
+
+    ! this function returns nothing
+    nret = 0
+    
+  end function script_get_dimensions
+
+end module m_genq_lua
+
 module m_genq
 
+  
   use precision, only: dp
   use sys,       only: die
 
@@ -27,22 +183,56 @@ module m_genq
     ! q(1), q(2), ... q(M)
     ! this routine specifies the values of the crystal coordinates xa
 
-    ! It might be possible to generate this file automatically from
-    ! some suitable symbolic representation of the coordinates
-    ! This is just a proof of concept
+    use m_genq_lua, only: lua
+    use m_genq_lua, only: genq_q => q, genq_xa => xa
+    use m_genq_lua, only: genq_isa => isa
+    use m_genq_lua, only: genq_lua_nqs => nqs
+    use m_genq_lua, only: genq_lua_initialize
+    use m_genq_lua, only: genq_lua_initialized
 
+    use flook, only: lua_run
+    
     integer, parameter    :: p = selected_real_kind(10,100)
 
     integer, intent(in)                         :: M
-    real(p), intent(in), target                 :: q(M)
-    real(p), target, intent(inout)              :: xa(:,:)
+    real(p), intent(in)                         :: q(M)
+    real(p), intent(inout)                      :: xa(:,:)
     integer, intent(inout), optional            :: spec_no(:)
 
 
-    integer :: natoms
+    logical :: want_species_numbers
 
-    ! Make sure you include the right file
-    include "geom.inc"
+    if (.not. genq_lua_initialized) then
+       call genq_lua_initialize()
+       if (genq_lua_nqs /= M) then
+          call die("nqs mismatch in genq")
+       endif
+    endif
+
+    want_species_numbers = .false.
+    if (present(spec_no)) then
+       want_species_numbers = .true.
+    endif
+
+    ! Set the Fortran array and have Lua read it and put it
+    ! in its genq.q table
+    genq_q(:) = q(:)
+    call lua_run(lua, code = 'fortran_get_qs()')
+
+    ! Execute Lua function to generate xa and species tables
+    ! in genq table
+    call lua_run(lua, &
+         code = 'genq.xa, genq.species = coords_from_qs( genq.q )' )
+
+    ! Get xa array in the genq_lua module and copy it
+    call lua_run(lua, code = 'fortran_set_xa()')
+    xa(:,:) = genq_xa(:,:)
+
+    if (want_species_numbers) then
+       ! Get isa array in the genq_lua module and copy it
+       call lua_run(lua, code = 'fortran_set_isa()')
+       spec_no(:) = genq_isa(:)
+    endif
     
   end subroutine geom_from_genq
 
