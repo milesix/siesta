8d2fef43e794f888475f3df90a210d5f46bee6c5
Irina Lebedeva <ilebedeva@dosx0153.local>
2020-11-16 15:41:17 +0100

    Correction to script for running testsuite.

--- Tests/Scripts/tortilla.sh
+++ Tests/Scripts/tortilla.sh
@@ -5,22 +5,10 @@
 #BSUB -J prueba  # name of the job
 #BSUB -n 4       # number of processors
 
-echo Running on `hostname`
-##echo This jobs runs on the following processors:
-##echo `cat $PBS_NODEFILE`
-##NPROCS=`wc -l < $PBS_NODEFILE`
-##echo This job has allocated $NPROCS nodes
-#
-
-##cd $PBS_O_WORKDIR
-
 prog=../../../siesta
 
 make check tests="h2o_libomm" SIESTA="mpirun $prog" > Out.dat
 
-
-## Maybe:    
-## make SIESTA="mpirun --gm-kill 600 $prog"
 
 
 
