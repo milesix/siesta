c4bd6a4dd48a80bdc10130ccf9a590e7f97430fb
José María Escartín Esteban <jm.escartin@icn2.cat>
2021-03-25 17:21:55 +0100

    Siesta version: also consider numeric tags that do not start with v.
    This is in line with the expected drop of the 'v' for
    repository tags of Siesta 5 and higher.

--- SIESTA_vgen.sh
+++ SIESTA_vgen.sh
@@ -42,7 +42,7 @@ elif test -d ${GIT_DIR:-.git} -o -f .git &&
       # Only follow first parents (required given our recurring merges).
       # Only consider regular (v[0-9]*) and MaX tags, irrespective of whether they are annotated or not.
       Vnew=$(git describe --tags --first-parent --abbrev=9 \
-             --match "v[0-9]*" --match "MaX-*" HEAD 2>/dev/null) &&
+             --match "v[0-9]*" --match "[0-9].[0-9]*" --match "MaX-*" HEAD 2>/dev/null) &&
       case "${Vnew}" in
           *${LF}*) (exit 1)
               ;;
