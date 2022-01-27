6678ce7fe02bf1b19ca0b6f0499df22bf4139d4f
Vladimir Dikan <vdikan@vivaldi.net>
2022-01-28 00:31:07 +0100

    reverted to psi_hat computation in KS to Garcia-Ordejon

--- Src/thermal_flux_jks.f90
+++ Src/thermal_flux_jks.f90
@@ -63,16 +63,16 @@ contains
     real(dp) :: tr_hat, tr_shat
 
     real(dp), allocatable :: tmp_g(:)
-    real(dp), allocatable :: amat(:,:), amat_save(:,:)
-    real(dp), allocatable :: sinv_h_mat(:,:), dm_s_mat(:,:)
-    integer, allocatable :: ipiv(:)
-    real(dp)    :: alpha_reg = 0.001_dp ! To regularize (H-e)^-1
-    integer :: i, j,  info
+    ! real(dp), allocatable :: amat(:,:), amat_save(:,:)
+    ! real(dp), allocatable :: sinv_h_mat(:,:), dm_s_mat(:,:)
+    ! integer, allocatable :: ipiv(:)
+    ! real(dp)    :: alpha_reg = 0.001_dp ! To regularize (H-e)^-1
+    ! integer :: i, j,  info
 
     allocate (tmp_g(no_l))
-    allocate(ipiv(no_u), amat(no_u,no_u))
-    allocate(amat_save(no_u,no_u))
-    allocate(sinv_h_mat(no_u,no_u), dm_s_mat(no_u,no_u))
+    ! allocate(ipiv(no_u), amat(no_u,no_u))
+    ! allocate(amat_save(no_u,no_u))
+    ! allocate(sinv_h_mat(no_u,no_u), dm_s_mat(no_u,no_u))
 
     ! computing and storage of every of the 3 components
     ! should be available to order in the .fdf
@@ -81,7 +81,7 @@ contains
 
     ! Compute first P_c [H,r] |Psi_iw>
 
-    alpha_reg = fdf_get('alpha.reg',alpha_reg)
+    ! alpha_reg = fdf_get('alpha.reg',alpha_reg)
 
     do idx=1,3
        do iw = 1,n_wfs
@@ -93,7 +93,7 @@ contains
                 k = listhptr(nu) + ij
                 col = listh(k)
 
-                !! tmp_g(nu) = tmp_g(nu) + psi_base(col,iw) * Rmat_base(k,idx)
+                tmp_g(nu) = tmp_g(nu) + psi_base(col,iw) * Rmat_base(k,idx)
                 ! New equations: Use [H,r] trick and linear equations.
                 ! Gamma_mu_nu in former version of notes is now hr_commutator_mu_nu
                 ! (** take care of mu_nu vs nu_mu issues **)
@@ -102,7 +102,7 @@ contains
                 ! tmp_g(nu) = tmp_g(nu) + psi_base(col,iw) * gradS_base(idx,k)
 
                 ! Complete commutator
-                tmp_g(nu) = tmp_g(nu) + psi_base(col,iw) * hr_commutator(k,idx)
+                ! tmp_g(nu) = tmp_g(nu) + psi_base(col,iw) * hr_commutator(k,idx)
 
              end do
           end do
@@ -156,8 +156,8 @@ contains
        enddo
     end if
 
-    sinv_h_mat = matmul(Sinv,H_base)
-    dm_s_mat = matmul(DM_save(:,:,1,1),S_base)
+    ! sinv_h_mat = matmul(Sinv,H_base)
+    ! dm_s_mat = matmul(DM_save(:,:,1,1),S_base)
 
     do iw = 1,n_wfs
 
@@ -166,13 +166,13 @@ contains
        ! But note tensorial issues
     ! Use dgesv for AX=B, with the solution X overwriting B (just what we need)
 
-       amat = 0.0_dp
-       do i = 1, no_u
-          do j = 1, no_u
-             amat(i,j) = sinv_h_mat(i,j) + alpha_reg*dm_s_mat(i,j)
-          enddo
-          amat(i,i) = amat(i,i) - eo_base(iw)
-       enddo
+       ! amat = 0.0_dp
+       ! do i = 1, no_u
+       !    do j = 1, no_u
+       !       amat(i,j) = sinv_h_mat(i,j) + alpha_reg*dm_s_mat(i,j)
+       !    enddo
+       !    amat(i,i) = amat(i,i) - eo_base(iw)
+       ! enddo
 
 
 ! subroutine dgesv	(	integer 	N,
@@ -185,18 +185,18 @@ contains
 ! integer 	INFO
 ! )
 
-       ! Do each cartesian direction separately.
-       amat_save = amat
+       ! ! Do each cartesian direction separately.
+       ! amat_save = amat
 
        do idx = 1, 3
-          amat = amat_save ! amat is destroyed at each iteration...
-          call dgesv(no_u, 1, amat, no_u, ipiv, psi_hat_c(1,iw,idx), no_u, info)
-          if (info < 0) then
-             write(0,*) "Argument had an illegal value: ", -info
-             call die("Error in dgesv call")
-          else if (info > 0) then
-             call die("A is singular in dgesv call")
-          endif
+          ! amat = amat_save ! amat is destroyed at each iteration...
+          ! call dgesv(no_u, 1, amat, no_u, ipiv, psi_hat_c(1,iw,idx), no_u, info)
+          ! if (info < 0) then
+          !    write(0,*) "Argument had an illegal value: ", -info
+          !    call die("Error in dgesv call")
+          ! else if (info > 0) then
+          !    call die("A is singular in dgesv call")
+          ! endif
 
           if (gk_setup%verbose_output) then
              ! Norm and scalar product with original wavefunction. The latter
@@ -216,7 +216,7 @@ contains
 
     enddo ! iw
 
-    deallocate(ipiv, amat, amat_save, sinv_h_mat, dm_s_mat)
+    ! deallocate(ipiv, amat, amat_save, sinv_h_mat, dm_s_mat)
 
   CONTAINS
 
