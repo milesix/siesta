9fc094a215a7c3f8ef6f81b55ff77f71815cd300
Alberto Garcia <albertog@icmab.es>
2022-02-13 22:26:24 +0100

    (wip) fdf uses client units table

--- Src/fdf/fdf.F90
+++ Src/fdf/fdf.F90
@@ -131,7 +131,24 @@ MODULE fdf
 
   USE utils
   USE prec
+
   implicit none
+
+  interface
+     function unit_conv_fac(from,to,stat,msg) result (factor)
+       import :: dp
+       character(len=*), intent(in)   :: from, to
+       integer, intent(out)           :: stat
+       character(len=96), intent(out) :: msg
+       real(dp)                       :: factor
+     end function unit_conv_fac
+  end interface
+
+#ifdef F2008
+  procedure(unit_conv_fac), pointer :: internal_convfac => no_unit_conversion
+#else
+  procedure(unit_conv_fac), pointer :: internal_convfac => null()
+#endif
 
 ! User callable routines in FDF library
 
@@ -142,7 +159,7 @@ MODULE fdf
   public :: fdf_get
   public :: fdf_integer, fdf_single, fdf_double
   public :: fdf_string, fdf_boolean
-  public :: fdf_physical, fdf_convfac
+  public :: fdf_physical
   public :: fdf_islist, fdf_list
 
 ! Returns the string associated with a mark line
@@ -169,6 +186,13 @@ MODULE fdf
   public :: fdf_bmatch, fdf_bsearch, fdf_substring_search
 
   public :: fdf_setoutput, fdf_setdebug
+
+  ! Units handling
+  ! The legacy name fdf_convfac is still exported, but
+  ! it is only usefulafter having set the
+  ! unit conversion function
+  public :: fdf_convfac
+  public :: fdf_set_unit_conversion_function
 
 ! Private functions, non-callable
 
@@ -2412,8 +2436,9 @@ MODULE fdf
         endif
 
         unitstr = names(mark%pline, 1, 2)
-        if (.not. leqi(unitstr, defunit))                               &
-          fdf_physical = value * fdf_convfac(unitstr, defunit)
+        if (.not. leqi(unitstr, defunit)) then
+           fdf_physical = value * fdf_convfac(unitstr, defunit)
+        endif
 
         if (fdf_output) write(fdf_out,'(a,5x,g20.10,1x,a10)') label, fdf_physical, defunit
         if (fdf_output) write(fdf_out,'(a,a,5x,g20.10,1x,a10)')                         &
@@ -2429,6 +2454,24 @@ MODULE fdf
       RETURN
 !--------------------------------------------------------------------------- END
     END FUNCTION fdf_physical
+
+    function fdf_convfac(from, to) result(factor)
+       character(len=*), intent(in)   :: from, to
+       real(dp)                       :: factor
+
+       integer :: stat
+       character(len=96) :: msg
+#ifndef F2008       
+       if (.not. associated(internal_convfac)) then
+          msg = 'no unit conversion table specified'
+          call die('FDF module: fdf_convfac', msg, THIS_FILE, __LINE__, fdf_err)
+       endif
+#endif       
+       factor = internal_convfac(from, to, stat, msg)
+       if (stat /= 0) then
+          call die('FDF module: fdf_convfac', msg, THIS_FILE, __LINE__, fdf_err)
+       endif
+     end function fdf_convfac
 
 !
 !   Returns a double precision value from a block-line after a certain input value
@@ -2489,166 +2532,6 @@ MODULE fdf
 
 !--------------------------------------------------------------------------- END
     END FUNCTION fdf_bphysical
-
-!
-!   Returns conversion factor between a subset of physical units
-!   Written by j.m.soler. dec'96.
-!   Modified by alberto garcia, jan'97.
-!   Added more units by Nick Papior, Aug'17.
-!
-    FUNCTION fdf_convfac(from, to)
-      implicit none
-!--------------------------------------------------------------- Input Variables
-      character(*)           :: from, to
-
-!-------------------------------------------------------------- Output Variables
-      real(dp)               :: fdf_convfac
-
-!--------------------------------------------------------------- Local Variables
-      character(80)          :: msg
-      integer(ip)            :: iu, ifrom, ito
-
-!------------------------------------------------------------------------- BEGIN
-
-!
-!     We allow case variations in the units. this could be dangerous
-!     (meV --> MeV!!) in real life, but not in this restricted 
-!     field.
-!     
-      integer(ip), parameter :: nu = 78
-      character(8) :: dimm(nu)
-      character(10) :: name(nu)
-      real(dp) :: unit(nu)
-      data (dimm(iu), name(iu), unit(iu), iu=1, 3) / &
-          'mass    ', 'g         ', 1.d-3, &
-          'mass    ', 'kg        ', 1.d0, &
-          'mass    ', 'amu       ', 1.66054d-27 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=4, 9) / &
-          'length  ', 'm         ', 1.d0, &
-          'length  ', 'cm        ', 1.d-2, &
-          'length  ', 'nm        ', 1.d-9, &
-          'length  ', 'pm        ', 1.d-12, &
-          'length  ', 'ang       ', 1.d-10, &
-          'length  ', 'bohr      ', 0.529177d-10 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=10, 19) / &
-          'energy  ', 'j         ', 1.d0, &
-          'energy  ', 'kj        ', 1.d3, &
-          'energy  ', 'erg       ', 1.d-7, &
-          'energy  ', 'mev       ', 1.60219d-22, &
-          'energy  ', 'ev        ', 1.60219d-19, &
-          'energy  ', 'mry       ', 2.17991d-21, &
-          'energy  ', 'ry        ', 2.17991d-18, &
-          'energy  ', 'mha       ', 4.35982d-21, &
-          'energy  ', 'mhartree  ', 4.35982d-21, &
-          'energy  ', 'ha        ', 4.35982d-18 /
-      data (dimm(iu), name(iu), unit(iu), iu=20, 29) / &
-          'energy  ', 'hartree   ', 4.35982d-18, &
-          'energy  ', 'k         ', 1.38066d-23, &
-          'energy  ', 'kelvin    ', 1.38066d-23, &
-          'energy  ', 'kcal/mol  ', 6.94780d-21, &
-          'energy  ', 'kj/mol    ', 1.6606d-21, &
-          'energy  ', 'hz        ', 6.6262d-34, &
-          'energy  ', 'thz       ', 6.6262d-22, &
-          'energy  ', 'cm-1      ', 1.986d-23, &
-          'energy  ', 'cm^-1     ', 1.986d-23, &
-          'energy  ', 'cm**-1    ', 1.986d-23 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=30, 39) / &
-          'time    ', 's         ', 1.d0, &
-          'time    ', 'ns        ', 1.d-9, &
-          'time    ', 'ps        ', 1.d-12, &
-          'time    ', 'fs        ', 1.d-15, &
-          'time    ', 'min       ', 60.d0, &
-          'time    ', 'mins      ', 60.d0, &
-          'time    ', 'hour      ', 3600.d0, &
-          'time    ', 'hours     ', 3600.d0, &
-          'time    ', 'day       ', 86400.d0, &
-          'time    ', 'days      ', 86400.d0 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=40, 43) / &
-          'force   ', 'n         ', 1.d0, &
-          'force   ', 'ev/ang    ', 1.60219d-9, &
-          'force   ', 'ry/bohr   ', 4.11943d-8, &
-          'force   ', 'ha/bohr   ', 8.23886d-08 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=44, 52) / &
-          'pressure', 'pa        ', 1.d0, &
-          'pressure', 'gpa       ', 1.d9, &
-          'pressure', 'atm       ', 1.01325d5, &
-          'pressure', 'bar       ', 1.d5, &
-          'pressure', 'mbar      ', 1.d11, &
-          'pressure', 'ev/ang**3 ', 1.60219d11, &
-          'pressure', 'ev/ang^3  ', 1.60219d11, &
-          'pressure', 'ry/bohr**3', 1.47108d13, &
-          'pressure', 'ry/bohr^3 ', 1.47108d13 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=53, 54) / &
-          'charge  ', 'c         ', 1.d0, &
-          'charge  ', 'e         ', 1.602177d-19 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=55, 59) / &
-          'dipole  ', 'c*m       ', 1.d0, &
-          'dipole  ', 'd         ', 3.33564d-30, &
-          'dipole  ', 'debye     ', 3.33564d-30, &
-          'dipole  ', 'e*bohr    ', 8.47835d-30, &
-          'dipole  ', 'e*ang     ', 1.602177d-29 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=60, 61) / &
-          'mominert', 'kg*m**2   ', 1.d0, &
-          'mominert', 'ry*fs**2  ', 2.17991d-48 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=62, 68) / &
-          'efield  ', 'v/m       ', 1.d0, &
-          'efield  ', 'v/nm      ', 1.d9, &
-          'efield  ', 'v/ang     ', 1.d10, &
-          'efield  ', 'v/bohr    ', 1.8897268d10, &
-          'efield  ', 'ry/bohr/e ', 2.5711273d11, &
-          'efield  ', 'ha/bohr/e ', 5.1422546d11, &
-          'efield  ', 'har/bohr/e', 5.1422546d11 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=69, 70) / &
-          'angle   ', 'deg       ', 1.d0, &
-          'angle   ', 'rad       ', 5.72957795d1 /
-
-      data (dimm(iu), name(iu), unit(iu), iu=71, 78) / &
-          'torque  ', 'mev/deg   ', 1.0d-3, &
-          'torque  ', 'mev/rad   ', 1.745533d-5, &
-          'torque  ', 'ev/deg    ', 1.0d0, &
-          'torque  ', 'ev/rad    ', 1.745533d-2, &
-          'torque  ', 'mry/deg   ', 13.6058d-3, &
-          'torque  ', 'mry/rad   ', 0.237466d-3, &
-          'torque  ', 'ry/deg    ', 13.6058d0, &
-          'torque  ', 'ry/rad    ', 0.237466d0 /
-
-!
-      ifrom = 0
-      ito   = 0
-      do iu= 1, nu
-        if (leqi(name(iu), from)) ifrom = iu
-        if (leqi(name(iu), to))   ito   = iu
-      end do
-
-      if (ifrom .eq. 0) then
-        write(msg,*) 'unknown unit = ', from
-        call die('FDF module: fdf_convfac', msg, THIS_FILE, __LINE__, fdf_err)
-      endif
-
-      if (ito .eq. 0) then
-        write(msg,*) 'unknown unit = ', to
-        call die('FDF module: fdf_convfac', msg, THIS_FILE, __LINE__, fdf_err)
-      endif
-
-      if (leqi(dimm(ifrom), dimm(ito))) then
-        fdf_convfac = unit(ifrom) / unit(ito)
-      else
-        write(msg,*) 'unit''s physical dimensions don''t match: ',      &
-                     from, ', ', to
-        call die('FDF module: fdf_convfac', msg, THIS_FILE, __LINE__, fdf_err)
-      endif
-!--------------------------------------------------------------------------- END
-    END FUNCTION fdf_convfac
 
 !
 !   Searches for label in the fdf hierarchy. If it appears the function
@@ -3388,4 +3271,27 @@ MODULE fdf
 !--------------------------------------------------------------------------- END
     END SUBROUTINE fdf_recvInput
 #endif
-END MODULE fdf
+
+    !
+    ! Units processing
+    !
+    subroutine fdf_set_unit_conversion_function(func)
+      procedure(unit_conv_fac) :: func
+
+      internal_convfac => func
+      
+    end subroutine fdf_set_unit_conversion_function
+
+    function no_unit_conversion(from,to,stat,msg) result (factor)
+        character(len=*), intent(in)   :: from, to
+        integer, intent(out)           :: stat
+        character(len=96), intent(out) :: msg
+        real(dp)                       :: factor
+
+        factor = 0.0_dp
+        stat = -1
+        msg = 'no unit conversion table specified'
+
+      end function no_unit_conversion
+
+  END MODULE fdf
--- Src/units.f90
+++ Src/units.f90
@@ -13,6 +13,9 @@ module units
   !                                 time: femtosecond
 
   use precision, only : dp
+!     Maybe separate leqi from fdf
+  use fdf, only: leqi
+
 
   implicit none
 
@@ -31,17 +34,174 @@ module units
 !  Ryd^-1 (time) = fs/0.04837769
 !   .... and so on.
 
-  real(dp), parameter :: Ang    = 1._dp / 0.529177_dp
-  real(dp), parameter :: eV     = 1._dp / 13.60580_dp
-  real(dp), parameter :: kBar   = 1._dp / 1.47108e5_dp
-  real(dp), parameter :: GPa    = kBar * 10
-  real(dp), parameter :: Kelvin = eV / 11604.45_dp
-  real(dp), parameter :: Debye  = 0.393430_dp
-  real(dp), parameter :: amu    = 2.133107_dp
-  real(dp), parameter :: Ryd_time = 1._dp/0.04837769_dp
+  ! For consistency, these should be better defined in terms of the units table.
+  ! Parameter initialization cannot use non-intrinsic functions, though.
+  ! These could be then made into 'protected variables' and initialized
+  ! in a call to a "units initialization function". 
+  
+  real(dp), parameter, public :: Ang    = 1._dp / 0.529177_dp
+  real(dp), parameter, public :: eV     = 1._dp / 13.60580_dp
+  real(dp), parameter, public :: kBar   = 1._dp / 1.47108e5_dp
+  real(dp), parameter, public :: GPa    = kBar * 10
+  real(dp), parameter, public :: Kelvin = eV / 11604.45_dp
+  real(dp), parameter, public :: Debye  = 0.393430_dp
+  real(dp), parameter, public :: amu    = 2.133107_dp
+  real(dp), parameter, public :: Ryd_time = 1._dp/0.04837769_dp
 
 ! pi to 50 digits
-  real(dp), parameter :: pi = 3.14159265358979323846264338327950288419716939937510_dp
-  real(dp), parameter :: deg = pi / 180.0_dp
+  real(dp), parameter, public :: pi = 3.14159265358979323846264338327950288419716939937510_dp
+  real(dp), parameter, public :: deg = pi / 180.0_dp
+
+      type, private :: unit
+        character(8) :: dimm
+        character(10) :: name
+        real(dp)      :: value
+      end type
+
+      type(unit), dimension(86), private, parameter :: table = [ &
+          unit('mass    ', 'g         ', 1.d-3), &
+          unit('mass    ', 'kg        ', 1.d0), &
+          unit('mass    ', 'amu       ', 1.66054d-27), &
+      !
+          unit('length  ', 'm         ', 1.d0), &
+          unit('length  ', 'cm        ', 1.d-2), &
+          unit('length  ', 'nm        ', 1.d-9), &
+          unit('length  ', 'pm        ', 1.d-12), &
+          unit('length  ', 'ang       ', 1.d-10), &
+          unit('length  ', 'bohr      ', 0.529177d-10), &
+      !
+          unit('energy  ', 'j         ', 1.d0), &
+          unit('energy  ', 'kj        ', 1.d3), &
+          unit('energy  ', 'erg       ', 1.d-7), &
+          unit('energy  ', 'mev       ', 1.60219d-22), &
+          unit('energy  ', 'ev        ', 1.60219d-19), &
+          unit('energy  ', 'mry       ', 2.17991d-21), &
+          unit('energy  ', 'ry        ', 2.17991d-18), &
+          unit('energy  ', 'mha       ', 4.35982d-21), &
+          unit('energy  ', 'mhartree  ', 4.35982d-21), &
+          unit('energy  ', 'ha        ', 4.35982d-18), &
+          unit('energy  ', 'hartree   ', 4.35982d-18), &
+          unit('energy  ', 'k         ', 1.38066d-23), &
+          unit('energy  ', 'kelvin    ', 1.38066d-23), &
+          unit('energy  ', 'kcal/mol  ', 6.94780d-21), &
+          unit('energy  ', 'kj/mol    ', 1.6606d-21), &
+          unit('energy  ', 'hz        ', 6.6262d-34), &
+          unit('energy  ', 'thz       ', 6.6262d-22), &
+          unit('energy  ', 'cm-1      ', 1.986d-23), &
+          unit('energy  ', 'cm^-1     ', 1.986d-23), &
+          unit('energy  ', 'cm**-1    ', 1.986d-23), &
+      !
+          unit('time    ', 's         ', 1.d0), &
+          unit('time    ', 'au        ', 2.418884326505e-17), &
+          unit('time    ', 'ns        ', 1.d-9), &
+          unit('time    ', 'ps        ', 1.d-12), &
+          unit('time    ', 'fs        ', 1.d-15), &
+          unit('time    ', 'min       ', 60.d0), &
+          unit('time    ', 'mins      ', 60.d0), &
+          unit('time    ', 'hour      ', 3600.d0), &
+          unit('time    ', 'hours     ', 3600.d0), &
+          unit('time    ', 'day       ', 86400.d0), &
+          unit('time    ', 'days      ', 86400.d0), &
+      !
+          unit('force   ', 'n         ', 1.d0), &
+          unit('force   ', 'ev/ang    ', 1.60219d-9), &
+          unit('force   ', 'ry/bohr   ', 4.11943d-8), &
+          unit('force   ', 'ha/bohr   ', 8.23886d-8), &
+      !
+          unit('pressure', 'pa        ', 1.d0), &
+          unit('pressure', 'gpa       ', 1.d9), &
+          unit('pressure', 'atm       ', 1.01325d5), &
+          unit('pressure', 'bar       ', 1.d5), &
+          unit('pressure', 'mbar      ', 1.d11), &
+          unit('pressure', 'ev/ang**3 ', 1.60219d11), &
+          unit('pressure', 'ev/ang^3  ', 1.60219d11), &
+          unit('pressure', 'ry/bohr**3', 1.47108d13), &
+          unit('pressure', 'ry/bohr^3 ', 1.47108d13), &
+          unit('pressure', 'ha/bohr^3 ', 2.94216d13), &
+          unit('pressure', 'ha/bohr**3', 2.94216d13), &
+      !
+          unit('surftens', 'n/m       ', 1.d0), &
+          unit('surftens', 'mn/m      ', 1.d3), &
+          unit('surftens', 'dyn/cm    ', 1.d3), &
+          unit('surftens', 'erg/cm**2 ', 1.d3), &
+      !
+          unit('charge  ', 'c         ', 1.d0), &
+          unit('charge  ', 'e         ', 1.602177d-19), &
+      !
+          unit('dipole  ', 'c*m       ', 1.d0), &
+          unit('dipole  ', 'd         ', 3.33564d-30), &
+          unit('dipole  ', 'debye     ', 3.33564d-30), &
+          unit('dipole  ', 'e*bohr    ', 8.47835d-30), &
+          unit('dipole  ', 'e*ang     ', 1.602177d-29), &
+      !
+          unit('mominert', 'kg*m**2   ', 1.d0), &
+          unit('mominert', 'ry*fs**2  ', 2.17991d-48), &
+      !
+          unit('efield  ', 'v/m       ', 1.d0), &
+          unit('efield  ', 'v/nm      ', 1.d9), &
+          unit('efield  ', 'v/ang     ', 1.d10), &
+          unit('efield  ', 'v/bohr    ', 1.8897268d10), &
+          unit('efield  ', 'ry/bohr/e ', 2.5711273d11), &
+          unit('efield  ', 'ha/bohr/e ', 5.1422546d11), &
+          unit('efield  ', 'har/bohr/e', 5.1422546d11), &
+      !
+          unit('angle   ', 'deg       ', 1.d0), &
+          unit('angle   ', 'rad       ', 5.72957795d1), &
+      !
+          unit('torque  ', 'mev/deg   ', 1.0d-3), &
+          unit('torque  ', 'mev/rad   ', 1.745533d-5), &
+          unit('torque  ', 'ev/deg    ', 1.0d0), &
+          unit('torque  ', 'ev/rad    ', 1.745533d-2), &
+          unit('torque  ', 'mry/deg   ', 13.6058d-3), &
+          unit('torque  ', 'mry/rad   ', 0.237466d-3), &
+          unit('torque  ', 'ry/deg    ', 13.6058d0), &
+          unit('torque  ', 'ry/rad    ', 0.237466d0), &
+      !
+      unit('dummy', 'dummy', 0.0) ]
+
+      private
+      public :: unit_conversion_factor
+
+    CONTAINS
+
+      function unit_conversion_factor(from,to,stat,msg) result (factor)
+        character(len=*), intent(in)   :: from, to
+        integer, intent(out)           :: stat
+        character(len=96), intent(out) :: msg
+        real(dp)                       :: factor
+        
+        integer :: ifrom, ito, iu
+
+        stat = 0
+        factor = 0.0_dp
+        
+        ifrom = 0
+        ito   = 0
+        do iu= 1, size(table)
+           if (leqi(table(iu)%name, from)) ifrom = iu
+           if (leqi(table(iu)%name, to))   ito   = iu
+        end do
+
+        if (ifrom .eq. 0) then
+           stat = 1
+           write(msg,*) 'unknown unit = ', from
+           return
+        endif
+        if (ito .eq. 0) then
+           stat = 2
+           write(msg,*) 'unknown unit = ', to
+           return
+        endif
+        if (leqi(table(ifrom)%dimm, table(ito)%dimm)) then
+           factor = table(ifrom)%value / table(ito)%value
+           return
+        else
+           stat = -1
+           write(msg,*) 'unit''s physical dimensions don''t match: ',      &
+                     from, ', ', to
+           return
+        endif
+        
+      end function unit_conversion_factor
 
 end module units
