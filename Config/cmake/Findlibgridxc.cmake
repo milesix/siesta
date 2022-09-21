a02c643db076090693219f7bca419e722ffce195
Alberto Garcia <albertog@icmab.es>
2022-09-21 17:14:06 +0200

    Allow pre-compiled cmake packages for libgridxc

--- Config/cmake/Findlibgridxc.cmake
+++ Config/cmake/Findlibgridxc.cmake
@@ -19,8 +19,9 @@ set(_pkg "LIBGRIDXC")
 set(_url "https://gitlab.com/siesta-project/libraries/libgridxc")
 set(_tag "cmake-master")
 
-# For now, allow only compilation as a subproject or by fetching
-set(LIBGRIDXC_FIND_METHOD "subproject" "fetch")
+# For now, allow only compilation as a subproject or by fetching,
+# or as a pre-compiled cmake-ready package
+set(LIBGRIDXC_FIND_METHOD "cmake" "subproject" "fetch")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
