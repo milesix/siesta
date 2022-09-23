af4da54da4298532a3f119896fa529f9f2bd15a9
Alberto Garcia <albertog@icmab.es>
2022-09-23 12:07:51 +0200

    Update compiler flags
    Use also a reporting device borrowed from the DFTB+ project.

--- Config/cmake/flags.cmake
+++ Config/cmake/flags.cmake
@@ -1,13 +1,62 @@
+#
+#  This can be improved.
+#  Toolchain files might also be useful for this
+#
+
+
 if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
-    set(CMAKE_Fortran_FLAGS         "${CMAKE_Fortran_FLAGS} -Wall")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-g -traceback")
-    set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -ip -xHOST")
+    message("Using an Intel compiler")
+    
+    if (NOT CMAKE_Fortran_FLAGS_RELEASE)
+      set(CMAKE_Fortran_FLAGS_RELEASE "-O2 -ip -xHOST")
+    endif()
+    if (NOT CMAKE_Fortran_FLAGS_RELWITHDEBINFO)
+      set(CMAKE_Fortran_FLAGS_RELWITHDEBINFO   "-g  ${CMAKE_Fortran_FLAGS_RELEASE} -traceback")
+    endif()
+    if (NOT CMAKE_Fortran_FLAGS_DEBUG)
+      set(CMAKE_Fortran_FLAGS_DEBUG  "-g -O0 -check -traceback")
+    endif()
+    
 endif()
 
 if(CMAKE_Fortran_COMPILER_ID MATCHES GNU)
     message("Using a GNU compiler")
-#    set(CMAKE_Fortran_FLAGS         "${CMAKE_Fortran_FLAGS} -Wall")
-#    set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g3")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-O0")
-    set(CMAKE_Fortran_FLAGS_RELEASE "-Ofast -march=native")
+ 
+    if ( CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 10.0 )
+      message(STATUS "  Adding '-fallow-argument-mismatch' for GNU >= 10.0")
+      set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fallow-argument-mismatch")
+    endif()
+
+    if (NOT CMAKE_Fortran_FLAGS_RELEASE)
+      set(CMAKE_Fortran_FLAGS_RELEASE "-Ofast -march=native")
+                      ## alt:    set(CMAKE_Fortran_FLAGS_RELEASE ""-O2 -funroll-all-loops")
+    endif()
+    if (NOT CMAKE_Fortran_FLAGS_RELWITHDEBINFO)
+      set(CMAKE_Fortran_FLAGS_RELWITHDEBINFO   "-g  ${CMAKE_Fortran_FLAGS_RELEASE} -traceback")
+    endif()
+    if (NOT CMAKE_Fortran_FLAGS_DEBUG)
+      set(CMAKE_Fortran_FLAGS_DEBUG  "-g -O0 -fbounds-check")
+    endif()
+
 endif()
+
+
+#
+# Following snippet borrowed from DFTB+ project
+#
+  if(CMAKE_BUILD_TYPE)
+    set(_buildtypes ${CMAKE_BUILD_TYPE})
+  else()
+    set(_buildtypes ${CMAKE_CONFIGURATION_TYPES})
+  endif()
+  foreach(_buildtype IN LISTS _buildtypes)
+    foreach (lang IN ITEMS Fortran C)
+      string(TOUPPER "${_buildtype}" _buildtype_upper)
+      message(STATUS "Flags for ${lang}-compiler (build type: ${_buildtype}): "
+        "${CMAKE_${lang}_FLAGS} ${CMAKE_${lang}_FLAGS_${_buildtype_upper}}")
+    endforeach()
+  endforeach()
+  unset(_buildtypes)
+  unset(_buildtype)
+  unset(_buildtype_upper)
+
