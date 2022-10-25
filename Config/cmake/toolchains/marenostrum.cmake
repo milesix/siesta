491001187e0d70eaa2e55969b7a73d57437145e8
Alberto Garcia <albertog@icmab.es>
2022-10-25 16:27:10 +0200

    Toolchains: update MN's and add Vega toolchain with GPU support
    * Fix variable value syntax in MareNostrum's toolchain file
    
    * Add Vega toolchain, including notes on compilation of ELPA with GPU
      support. Also, change the GPU name in diag.F90 to 'nvidia-gpu', to
      accomodate newer versions of ELPA. The support for GPU options needs
      to be extended, to cover cases other than Nvidia GPUs, and to
      maintain compatibility with older versions of ELPA.

--- Config/cmake/toolchains/marenostrum.cmake
+++ Config/cmake/toolchains/marenostrum.cmake
@@ -1,7 +1,7 @@
 #
 # Marenostrum with Intel 2017.4 toolchain. NO OPENMP
 #
-set(WITH_OMP OFF CACHE BOOL "OpenMP setting")
+set(WITH_OMP "OFF" CACHE BOOL "OpenMP setting")
 
 set(LAPACK_LIBRARY
     "-L/apps/INTEL/2017.4/mkl/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core"
