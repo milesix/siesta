ae96098202ff32b1041a022b7574bc815f03411a
Alberto Garcia <albertog@icmab.es>
2022-12-06 16:25:45 +0100

    enh: Improvements in CMake framework
     * Improve tests in Util/MPI_Test
     * More informative NetCDF diagnostics
     * Fix OpenMP building in Util/Optimizer
        (Thanks to JM Escartin)
     * Add Contrib/APostnikov to build
     * Simplify the search for linear-algebra routines
    
        * Remove the explict search for BLAS.
        * Clarify the use of the "NONE" setting for (SCA)LAPACK_LIBRARY
        * Add skeleton for default search directories for ScaLapack
          (borrowed from SIRIUS)

--- Config/cmake/toolchains/mac.cmake
+++ Config/cmake/toolchains/mac.cmake
@@ -1,9 +1,7 @@
 set(LAPACK_LIBRARY "-lveclibfort" CACHE STRING "lapack library chosen")
-set(BLAS_LIBRARY "NONE" CACHE STRING "blas library chosen")
 set(SCALAPACK_LIBRARY "$ENV{SCALAPACK_LIBS}" CACHE STRING "scalapack library chosen")
 set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "build_type")
 set(Fortran_FLAGS_DEBUG  "-g -O0" CACHE STRING "Fortran debug flags")
 
-#  set(BLAS_DETECTION OFF CACHE BOOL "BLAS detection")
 #  set(LAPACK_DETECTION OFF CACHE BOOL "LAPACK detection")
 
--- Config/cmake/toolchains/marenostrum.cmake
+++ Config/cmake/toolchains/marenostrum.cmake
@@ -7,8 +7,6 @@ set(LAPACK_LIBRARY
     "-L/apps/INTEL/2017.4/mkl/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core"
         CACHE STRING "lapack library chosen")
 	
-set(BLAS_LIBRARY "NONE" CACHE STRING "blas library chosen")
-
 set(SCALAPACK_LIBRARY
     "-L/apps/INTEL/2017.4/mkl/lib/intel64 -lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core"
      CACHE STRING "scalapack library chosen")
