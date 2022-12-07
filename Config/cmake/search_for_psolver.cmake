41669c45b2e5f0c5c5c9fe8bed435105e4eb9286
Alberto Garcia <albertog@icmab.es>
2022-12-07 20:41:06 +0100

    Add warning about stray libraries

--- Config/cmake/search_for_psolver.cmake
+++ Config/cmake/search_for_psolver.cmake
@@ -8,9 +8,15 @@
  message(DEBUG "PSOLVER Libdir: ${PSOLVER_LIBDIR}")
  message(DEBUG "Psolver Libraries: ${PSOLVER_LIBRARIES}")
  message(STATUS "Psolver Link libraries: ${PSOLVER_LINK_LIBRARIES}")
+
+ message(STATUS " ----- ")
+ message(STATUS "   ** Make sure that there are no stray linear algebra libraries **")
+ message(STATUS "   ** in the above list of libraries                             **")
+ message(STATUS " ----- ")
+ 
  message(DEBUG "PSOLVER_INCLUDEDIR: ${PSOLVER_INCLUDEDIR}")
  message(STATUS "PSOLVER_INCLUDE_DIRS: ${PSOLVER_INCLUDE_DIRS}")
 
- add_library(psolver::psolver INTERFACE IMPORTED GLOBAL)
+add_library(psolver::psolver INTERFACE IMPORTED GLOBAL)
  target_link_libraries(psolver::psolver INTERFACE  ${PSOLVER_LINK_LIBRARIES})
  target_include_directories(psolver::psolver INTERFACE ${PSOLVER_INCLUDE_DIRS})
