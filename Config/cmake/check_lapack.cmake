7ceedad2b9316ffa0d162360b986fc50f5d81166
Nick Papior <nickpapior@gmail.com>
2023-03-21 13:46:07 +0100

    cleaned BLAS+LAPACK checks for easier understanding of problem
    The LAPACK references was ill-placed. Some of the tests were
    BLAS. They are now split up and change message to a WARNING
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- Config/cmake/check_lapack.cmake
+++ Config/cmake/check_lapack.cmake
@@ -7,29 +7,36 @@ include(CheckFortranSourceCompiles)
 include(CheckFortranSourceRuns)
 
 set(CMAKE_REQUIRED_QUIET OFF)
-message(STATUS "Checking that Lapack library links...")
-	
-set(CMAKE_REQUIRED_LIBRARIES LAPACK::LAPACK)
-check_fortran_source_compiles(
-"
-external :: sgemm, dsysv
-call sgemm()
-call dsysv()
-end
-"
-LAPACK_LINKS_OK SRC_EXT F90)
 
-unset(CMAKE_REQUIRED_LIBRARIES)
 
-if (NOT LAPACK_LINKS_OK)
- message(STATUS "  ---------------------------------------------")
- message(STATUS "  *** LAPACK library does not link properly")
- message(STATUS "  Please check the library linking string found or used by CMake")
- message(STATUS "  ---------------------------------------------")
- message(FATAL_ERROR "  *** LAPACK library does not link properly")
+message(STATUS "Checking that BLAS library works...")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+# Check that sgemm can be found (blas check)
+if( TARGET BLAS::BLAS )
+  set(CMAKE_REQUIRED_LIBRARIES BLAS::BLAS)
+else()
+  set(CMAKE_REQUIRED_LIBRARIES LAPACK::LAPACK)
 endif()
 
-set(CMAKE_REQUIRED_LIBRARIES LAPACK::LAPACK)
+check_fortran_source_compiles(
+"
+external :: sgemm
+call sgemm()
+end
+"
+  blas_has_sgemm SRC_EXT F90)
+
+if (NOT blas_has_sgemm)
+  message(WARNING
+    "---------------------------------------------"
+    " BLAS library cannot link properly"
+    " Please check the library linking string found or used by CMake"
+    "---------------------------------------------")
+  message(FATAL_ERROR " BLAS library does not link properly")
+endif()
+
+
 check_fortran_source_runs(
 "
 complex :: c
@@ -39,20 +46,52 @@ external :: cdotu
 c=cdotu(2,a(:),1,a(:),1)
 end
 "
-LAPACK_RETURN_CONVENTION_OK SRC_EXT F90)
+blas_cdotu_return_convention SRC_EXT F90)
 unset(CMAKE_REQUIRED_LIBRARIES)
 
-if (NOT LAPACK_RETURN_CONVENTION_OK)
- message(STATUS "  ---------------------------------------------")
- message(STATUS "  *** LAPACK library uses wrong return-value convention!!!")
- message(STATUS "  This is likely to happen on MacOS if the default Accelerate framework is used")
- message(STATUS "  You can install veclibfort (https://github.com/mcg1969/vecLibFort)")
- message(STATUS "  and then set -DLAPACK_LIBRARY=-lveclibfort in your cmake invocation")
- message(STATUS "  You can also set -DBLAS_LIBRARY=-lveclibfort, but it is not strictly needed")
- message(STATUS "    (The key is that -lveclibfort appears first in the link stage,")
- message(STATUS "     so the BLAS library can still point to the raw Accelerate")
- message(STATUS "  ")
- message(STATUS "  Alternatively you can install OpenBLAS and set the variables accordingly")
- message(STATUS "  ---------------------------------------------")
- message(FATAL_ERROR "  *** LAPACK library uses wrong return-value convention!!!")
+if (NOT blas_cdotu_return_convention)
+  message(WARNING
+    "---------------------------------------------"
+    " BLAS library uses wrong return-value convention!"
+    " This is likely to happen on MacOS if the default Accelerate framework is used"
+    " You can install veclibfort (https://github.com/mcg1969/vecLibFort)"
+    " and then set either of:\n"
+    "   -DBLAS_LIBRARY=-lveclibfort"
+    " or"
+    "   -DLAPACK_LIBRARY=-lveclibfort\n"
+    " in your cmake invocation."
+    " Where the key point is to have the BLAS library linked first."
+    " Alternatively you can install OpenBLAS/BLIS and set the variables accordingly."
+    "---------------------------------------------")
+  message(FATAL_ERROR " BLAS library uses wrong return-value convention!!!")
 endif()
+
+list(POP_BACK CMAKE_MESSAGE_INDENT)
+
+
+message(STATUS "Checking that LAPACK library works...")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+# Test for lapack
+set(CMAKE_REQUIRED_LIBRARIES LAPACK::LAPACK)
+check_fortran_source_compiles(
+"
+external :: dsysv
+call dsysv()
+end
+"
+lapack_has_dsysv SRC_EXT F90)
+
+if (NOT lapack_has_dsysv)
+  message(WARNING
+    "---------------------------------------------"
+    " LAPACK library cannot link properly"
+    " Please check the library linking string found or used by CMake"
+    "---------------------------------------------")
+ message(FATAL_ERROR "  *** LAPACK library does not link properly")
+endif()
+
+
+unset(CMAKE_REQUIRED_LIBRARIES)
+
+list(POP_BACK CMAKE_MESSAGE_INDENT)
