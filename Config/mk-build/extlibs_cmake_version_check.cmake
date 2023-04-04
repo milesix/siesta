08661e19cd374363b690c371a6e2a27d542986db
Alberto Garcia <albertog@icmab.es>
2023-04-04 15:28:20 +0200

    Mostly cosmetic changes to build-related files

--- Config/mk-build/extlibs_cmake_version_check.cmake
+++ Config/mk-build/extlibs_cmake_version_check.cmake
@@ -1,3 +1,3 @@
-if(${CMAKE_VERSION} VERSION_LESS "3.17.0")
-    message( FATAL_ERROR "On-the-fly dependency compilation needs CMake >= 3.17")
+if(${CMAKE_VERSION} VERSION_LESS "3.14.0")
+    message( FATAL_ERROR "On-the-fly dependency compilation needs CMake >= 3.14")
 endif()
