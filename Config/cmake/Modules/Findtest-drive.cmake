5cd0444fc33c9a7d25c273f0f603a05d91b8166a
Alberto Garcia <albertog@icmab.es>
2023-06-27 19:38:40 +0200

    Promote s-dftd3 target to GLOBAL + required flag for test-drive

--- Config/cmake/Modules/Findtest-drive.cmake
+++ Config/cmake/Modules/Findtest-drive.cmake
@@ -1,6 +1,7 @@
 include(SiestaFindPackage)
 
 Siesta_find_package(test-drive
+  REQUIRED
   GIT_REPOSITORY "https://github.com/fortran-lang/test-drive"
   GIT_TAG "v0.4.0"
   SOURCE_DIR ${PROJECT_SOURCE_DIR}/External/DFTD3/test-drive
