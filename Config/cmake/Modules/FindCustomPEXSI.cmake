e6da5567445ece2201650054dfb3eb8781aad0e3
Alberto Garcia <albertog@icmab.es>
2023-07-12 10:57:33 +0200

    More refinements to search for PEXSI 2.1

--- Config/cmake/Modules/FindCustomPEXSI.cmake
+++ Config/cmake/Modules/FindCustomPEXSI.cmake
@@ -8,6 +8,12 @@
 
 include(FindPackageHandleStandardArgs)
 
+# In pre-release 2.1 the (static) libraries are all
+# in ${PEXSI_ROOT}/lib
+# ... but we provide extra hooks for other places
+# for the subordinate libraries
+#
+
 find_library(PEXSI_LIBRARIES
   NAMES pexsi
   PATH_SUFFIXES lib
@@ -20,6 +26,7 @@ find_library(PARMETIS_LIBRARIES
   PATH_SUFFIXES lib
   HINTS
   ENV PEXSI_ROOT
+  ENV PARMETIS_ROOT
   DOC "Parmetis libraries list")
 
 find_library(METIS_LIBRARIES
@@ -27,13 +34,15 @@ find_library(METIS_LIBRARIES
   PATH_SUFFIXES lib
   HINTS
   ENV PEXSI_ROOT
+  ENV METIS_ROOT
   DOC "Metis libraries list")
 
-find_library(SUPERLU_LIBRARIES
+find_library(SUPERLU_DIST_LIBRARIES
   NAMES superlu_dist
   PATH_SUFFIXES lib
   HINTS
   ENV PEXSI_ROOT
+  ENV SUPERLU_DIST_ROOT
   DOC "SuperLU_dist libraries list")
 
 find_path(PEXSI_INCLUDE_DIR
@@ -44,31 +53,59 @@ find_path(PEXSI_INCLUDE_DIR
 
 find_package_handle_standard_args(CustomPEXSI "DEFAULT_MSG"
                                   PEXSI_LIBRARIES PARMETIS_LIBRARIES METIS_LIBRARIES
-				  SUPERLU_LIBRARIES PEXSI_INCLUDE_DIR)
+				  SUPERLU_DIST_LIBRARIES PEXSI_INCLUDE_DIR)
 
 
 message("PEXSI_INCLUDE_DIR: ${PEXSI_INCLUDE_DIR}")
 message("PEXSI_LIBRARIES: ${PEXSI_LIBRARIES}")
 message("PARMETIS_LIBRARIES: ${PARMETIS_LIBRARIES}")
 message("METIS_LIBRARIES: ${PARMETIS_LIBRARIES}")
+message("SUPERLU_LIBRARIES: ${SUPERLU_DIST_LIBRARIES}")
 
 
 if(CustomPEXSI_FOUND AND NOT TARGET PEXSI::PEXSI)
 
+
+# Instead of 'STATIC' (the current setting for pre-release 2.1),
+# we might want to use 'SHARED' if appropriate (to be implemented)
+
+  add_library(pexsi STATIC IMPORTED)
+  set_target_properties(pexsi PROPERTIES
+  			      IMPORTED_LOCATION "${PEXSI_LIBRARIES}"
+                              IMPORTED_LINK_INTERFACE_LANGUAGES "CXX")
+  add_library(parmetis STATIC IMPORTED)
+  set_target_properties(parmetis PROPERTIES
+  			      IMPORTED_LOCATION "${PARMETIS_LIBRARIES}"
+                              IMPORTED_LINK_INTERFACE_LANGUAGES "CXX")
+  add_library(metis STATIC IMPORTED)
+  set_target_properties(metis PROPERTIES
+  			      IMPORTED_LOCATION "${METIS_LIBRARIES}"
+                              IMPORTED_LINK_INTERFACE_LANGUAGES "CXX")
+  add_library(superlu_dist STATIC IMPORTED)
+  set_target_properties(superlu_dist PROPERTIES
+  			      IMPORTED_LOCATION "${SUPERLU_DIST_LIBRARIES}"
+                              IMPORTED_LINK_INTERFACE_LANGUAGES "CXX")
+
+  
   add_library(PEXSI::PEXSI INTERFACE IMPORTED)
 
   # I need to add by hand "-lstdc++"
   # This is non-optimal, since other platforms might have a different
   # name for the C++ library
-  
-  set_target_properties(PEXSI::PEXSI PROPERTIES
-                                     INTERFACE_INCLUDE_DIRECTORIES "${PEXSI_INCLUDE_DIR}"
-				     IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
-                                     INTERFACE_LINK_LIBRARIES
-		 "${PEXSI_LIBRARIES};${PARMETIS_LIBRARIES};${METIS_LIBRARIES};${SUPERLU_LIBRARIES};-lstdc++")
 
-  # This does not seem to be needed
-  target_link_libraries(PEXSI::PEXSI INTERFACE MPI::MPI_CXX)
+  target_link_libraries(PEXSI::PEXSI
+                          INTERFACE
+                          pexsi
+			  parmetis
+			  metis
+			  superlu_dist)
+			  
+  set_target_properties(PEXSI::PEXSI
+                        PROPERTIES
+                        INTERFACE_INCLUDE_DIRECTORIES "${PEXSI_INCLUDE_DIR}")
+
+  # This does not seem to be needed, but YMMV
+  #  target_link_libraries(PEXSI::PEXSI INTERFACE MPI::MPI_CXX)
   
 endif()
 
